fcd9d637fe5dd2062c83953209eb169446236ef9
Ridwan Taiwo <donriddo@gmail.com>
2019-04-06 04:56:15 +0100

    Change solve.moduleDirectories to resolve.modules in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'development',
     resolve: {
         extensions: ['.js', '.jsx', '.css'],
-        modulesDirectories: [
+        modules: [
             'node_modules'
         ]
     },
